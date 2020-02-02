9eb5d845b50a7c54f6ae44b916df11ac75eb8e8d
Manuel Delgado <mdelgado@27@miners.utep.edu>
2020-02-02 14:29:02 -0700

    Seperated creation of window and content.

--- GUI/gui_add_vector_pop_up.py
+++ GUI/gui_add_vector_pop_up.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import (QWidget, QDesktopWidget, QLabel, QLineEdit, QTextEdit, QGridLayout)
+from PyQt5.QtWidgets import (QWidget, QDesktopWidget, QLabel, QLineEdit, QTextEdit, QGridLayout, QFrame, QProgressBar)
 from PyQt5.QtGui import QIcon
 
 
@@ -13,17 +13,25 @@ class AddVectorPopUp(QWidget):
         self.setWindowTitle("Add Vector")
         self.setWindowIcon(QIcon("../Resources/Images/add-square.png"))
         self.set_window()
+        self.create_body()
         self.show()
 
     def set_window(self):
-        v_name = QLabel("Name*")
-        v_description = QLabel("Description")
+        window = self.frameGeometry()
+        win_center = QDesktopWidget().availableGeometry().center()
+        window.moveCenter(win_center)
+        self.move(window.topLeft())
+        self.setStyleSheet('background-color:#333;color:#ccc')
+
+    def create_body(self):
+        v_name = QLabel("Name* :")
+        v_description = QLabel("Description:")
 
         e_v_name = QLineEdit()
         e_v_description = QTextEdit()
 
         grid = QGridLayout()
-        grid.setSpacing(10)
+        #grid.setSpacing(10)
 
         grid.addWidget(v_name, 1, 0)
         grid.addWidget(e_v_name, 1, 1)
@@ -33,7 +41,4 @@ class AddVectorPopUp(QWidget):
 
         self.setLayout(grid)
 
-        window = self.frameGeometry()
-        win_center = QDesktopWidget().availableGeometry().center()
-        window.moveCenter(win_center)
-        self.move(window.topLeft())
+
