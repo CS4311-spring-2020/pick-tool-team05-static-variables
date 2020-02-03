1d58c0c60dfd29d7bf504529fa34e7f096343d9a
aleonal <thelifeofantoine@outlook.com>
2020-02-02 18:34:34 -0700

    Corrected button labels and icons to adjust for new project structure.

--- GUI/gui_main.py
+++ GUI/gui_main.py
@@ -1,11 +1,11 @@
 import sys
 
 from PyQt5.QtCore import QSize
-from PyQt5.QtWidgets import QDesktopWidget, QMainWindow, QAction, qApp, QMenu, QWidget, QSizePolicy
+from PyQt5.QtWidgets import QDesktopWidget, QMainWindow, QAction, QApplication, QMenu, QWidget, QSizePolicy
 from PyQt5.QtGui import QIcon
 
 
-class Root(QMainWindow):
+class gui_main(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initRoot()
@@ -13,7 +13,7 @@ class Root(QMainWindow):
     def initRoot(self):
         self.statusBar().showMessage('Ready')
         self.setWindowTitle('PMR Insight Collective Knowledge')
-        self.setWindowIcon(QIcon('../Resources/Images/icon.png'))
+        self.setWindowIcon(QIcon('Resources/Images/icon.png'))
 
         self.initSize()
         self.initMenuBar()
@@ -36,13 +36,13 @@ class Root(QMainWindow):
 
     def setFileMenu(self, file_menu):
         # (TODO): Add triggers
-        connect_act = QAction(QIcon('connect.png'), '&Connect...', self)
+        connect_act = QAction('&Connect...', self)
         connect_act.setShortcut('Ctrl+Shift+C')
         connect_act.setStatusTip('Connect to host')
         file_menu.addAction(connect_act)
 
         # (TODO): Add triggers
-        disconnect_act = QAction(QIcon('disconnect.png'), '&Disconnect...', self)
+        disconnect_act = QAction('&Disconnect...', self)
         disconnect_act.setShortcut('Ctrl+Shift+D')
         disconnect_act.setStatusTip('Disconnect from host')
         file_menu.addAction(disconnect_act)
@@ -50,28 +50,28 @@ class Root(QMainWindow):
         file_menu.addSeparator()
 
         # (TODO): Add triggers
-        files_act = QAction(QIcon('files.png'), '&View Log Files...', self)
+        files_act = QAction('&View Log Files...', self)
         files_act.setShortcut('Ctrl+Shift+V')
         files_act.setStatusTip('View log files')
         file_menu.addAction(files_act)
 
         file_menu.addSeparator()
 
-        exit_act = QAction(QIcon('exit.png'), '&Exit', self)
+        exit_act = QAction('&Exit', self)
         exit_act.setShortcut('Ctrl+Q')
         exit_act.setStatusTip('Exit application')
-        exit_act.triggered.connect(qApp.quit)
+        exit_act.triggered.connect(QApplication.quit)
         file_menu.addAction(exit_act)
 
     def setEditMenu(self, edit_menu):
         # (TODO): Add triggers
-        add_act = QAction(QIcon('add.png'), '&Add Vector...', self)
+        add_act = QAction('&Add Vector...', self)
         add_act.setShortcut('Ctrl+Shift+N')
         add_act.setStatusTip('Add vector')
         edit_menu.addAction(add_act)
 
         # (TODO): Add triggers
-        del_act = QAction(QIcon('delete.png'), '&Delete Vector', self)
+        del_act = QAction('&Delete Vector', self)
         del_act.setShortcut('Ctrl+Shift+D')
         del_act.setStatusTip('Delete vector')
         edit_menu.addAction(del_act)
@@ -79,7 +79,7 @@ class Root(QMainWindow):
         edit_menu.addSeparator()
 
         # (TODO): Add triggers
-        export_act = QAction(QIcon('export.png'), '&Export Graph...', self)
+        export_act = QAction('&Export Graph...', self)
         export_act.setShortcut('Ctrl+Shift+E')
         export_act.setStatusTip('Export graph')
         edit_menu.addAction(export_act)
@@ -89,37 +89,37 @@ class Root(QMainWindow):
         timeline = QMenu('Timeline Interval', self)
 
         # (TODO): Triggers in toggleGraphView
-        orientation_h = QAction('Horizontal', self)
+        orientation_h = QAction('&Horizontal', self)
         orientation_h.setStatusTip('Position graph horizontally')
         orientation_h.setCheckable(True)
         orientation_h.triggered.connect(self.toggleGraphView)
 
         # (TODO): Triggers in toggleGraphView
-        orientation_v = QAction('Vertical', self)
+        orientation_v = QAction('&Vertical', self)
         orientation_v.setStatusTip('Position graph vertically')
         orientation_v.setCheckable(True)
         orientation_v.triggered.connect(self.toggleGraphView)
 
         # (TODO): Triggers in zoom
-        seconds_i = QAction('Seconds', self)
+        seconds_i = QAction('&Seconds', self)
         seconds_i.setStatusTip('Set timeline interval to seconds')
         seconds_i.setCheckable(True)
         seconds_i.triggered.connect(self.zoom)
 
         # (TODO): Triggers in zoom
-        minutes_i = QAction('Minutes', self)
+        minutes_i = QAction('&Minutes', self)
         minutes_i.setStatusTip('Set timeline interval to minutes')
         minutes_i.setCheckable(True)
         minutes_i.triggered.connect(self.zoom)
 
         # (TODO): Triggers in zoom
-        hours_i = QAction('Hours', self)
+        hours_i = QAction('&Hours', self)
         hours_i.setStatusTip('Set timeline interval to hours')
         hours_i.setCheckable(True)
         hours_i.triggered.connect(self.zoom)
 
         # (TODO): Triggers in zoom
-        days_i = QAction('Days', self)
+        days_i = QAction('&Days', self)
         days_i.setStatusTip('Set timeline interval to hours')
         days_i.setCheckable(True)
         days_i.triggered.connect(self.zoom)
@@ -135,48 +135,47 @@ class Root(QMainWindow):
 
     def setHelpMenu(self, help_menu):
         # About button, missing trigger
-        help_act = QAction(QIcon('help.png'), '&Help', self)
+        help_act = QAction('&Help', self)
         help_act.setShortcut('f1')
         help_act.setStatusTip('Show Tips')
         help_menu.addAction(help_act)
 
         help_menu.addSeparator()
 
-        about_act = QAction(QIcon('question.png'), '&About', self)
+        about_act = QAction('&About', self)
         about_act.setStatusTip('About PMR Insight Collective Knowledge Tool')
         help_menu.addAction(about_act)
 
-    # (TODO): Add more icons according to future developments
+    # (TODO): Add more buttons according to future developments
     # (TODO): Complete adding basic icons
     def initToolBar(self):
         toolbar = self.addToolBar('Toolbar')
         toolbar.setMovable(False)
         toolbar.setIconSize(QSize(20, 20))
-        toolbar.setStyleSheet("QToolBar{padding-left:0px;}")
 
-        undo_act = QAction(QIcon('../Resources/Images/undo.png'), '&Undo', self)
+        undo_act = QAction(QIcon('Resources/Images/undo.png'), '&Undo', self)
         undo_act.setShortcut('Ctrl+Z')
         undo_act.setStatusTip('Undo action')
         toolbar.addAction(undo_act)
 
-        redo_act = QAction(QIcon('../Resources/Images/redo.png'), '&Redo', self)
+        redo_act = QAction(QIcon('Resources/Images/redo.png'), '&Redo', self)
         redo_act.setShortcut('Ctrl+Shift+Z')
         redo_act.setStatusTip('Redo action')
         toolbar.addAction(redo_act)
 
         toolbar.addSeparator()
 
-        refresh_act = QAction(QIcon('../Resources/Images/button-refresh-arrows.png'), '&Refresh', self)
+        refresh_act = QAction(QIcon('Resources/Images/button-refresh-arrows.png'), '&Refresh', self)
         refresh_act.setShortcut('Ctrl+R')
         refresh_act.setStatusTip('Refresh project')
         toolbar.addAction(refresh_act)
 
-        commit_act = QAction(QIcon('../Resources/Images/check-1.png'), '&Commit', self)
+        commit_act = QAction(QIcon('Resources/Images/check-1.png'), '&Commit', self)
         commit_act.setShortcut('Ctrl+Shift+C')
         commit_act.setStatusTip('Commit changes')
         toolbar.addAction(commit_act)
 
-        push_act = QAction(QIcon('../Resources/Images/share-1.png'), '&Push', self)
+        push_act = QAction(QIcon('Resources/Images/share-1.png'), '&Push', self)
         push_act.setShortcut('Ctrl+Shift+P')
         push_act.setStatusTip('Push commit')
         toolbar.addAction(push_act)
@@ -186,10 +185,10 @@ class Root(QMainWindow):
         spacer.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         toolbar.addWidget(spacer)
 
-    # (TODO): Complete
-    # def toggleGraphView(self):
-    #     print('hi')
+    #(TODO): Complete
+    def toggleGraphView(self):
+        print('hi')
 
-    # (TODO): Complete
-    # def zoom(self):
-    #     print('hello')
+    #(TODO): Complete
+    def zoom(self):
+        print('hello')
--- __init__.py
+++ __init__.py
@@ -2,7 +2,9 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 #To test stuff, call your GUIs here in your branch. If you merge to master, make sure to leave the file like this.
+from GUI.gui_main import gui_main
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    test = gui_main()
     sys.exit(app.exec_())
