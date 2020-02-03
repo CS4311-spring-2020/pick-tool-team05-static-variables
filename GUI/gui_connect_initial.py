628092a89003d48073416a45136d1036f8871429
Elizabeth Barragan <ebarragan3@Miners.utep.edu>
2020-02-02 17:50:52 -0700

    Added files

--- GUI/gui_connect_initial.py
+++ GUI/gui_connect_initial.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
 from PyQt5.QtWidgets import QWidget, QPushButton, QDesktopWidget, QLineEdit, QMessageBox
 from PyQt5 import QtWidgets
 
@@ -11,10 +8,8 @@ class ConnectHost(QWidget):
         super(ConnectHost, self).__init__()
         self.ui()
 
-    def connect_error_message(self):
-        QMessageBox.about(self, "Error", "The system did not find a host in the network")
-
     def ui(self):
+        # Window Dimensions
         self.resize(500, 250)
         self.center()
         self.setWindowTitle('Connect to Host')
@@ -32,11 +27,9 @@ class ConnectHost(QWidget):
         # Buttons in Window
         self.scan_button = QPushButton('Scan for Hosts', self)
         self.scan_button.move(110, 160)
-
-        self.scan_button.clicked.connect(self.connect_error_message)  # Message box
-
+        self.scan_button.clicked.connect(self.connect_error_message)
         self.cancel_button = QPushButton('Cancel', self)
-        self.cancel_button.move(290,160)
+        self.cancel_button.move(290, 160)
         self.cancel_button.clicked.connect(self.close)
 
         self.show()
@@ -47,13 +40,13 @@ class ConnectHost(QWidget):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
-
-
-
-
-
-
-
-
+    def connect_error_message(self):
+        msg = QMessageBox()
+        msg.setWindowTitle("Error")
+        msg.setText("The system could not connect to a network")
+        msg.setStandardButtons(QMessageBox.Ignore | QMessageBox.Retry)
+        msg.setDefaultButton(QMessageBox.Ignore)
+        # msg.buttonClicked.connect()    TODO: Connect to Main Page of app
+        x = msg.exec()
 
 
--- GUI/gui_eventconfig_error.py
+++ GUI/gui_eventconfig_error.py
@@ -1,12 +1,21 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QLineEdit, QMessageBox, QLineEdit, QPushButton, QDesktopWidget
 
 
 class EventConfigError(QWidget):
 
     def __init__(self):
         super(EventConfigError, self).__init__()
+        self.ui()
 
+    def ui(self):
 
-   # def ui(self):
+        self.resize(550, 300)
+        self.textbox = QLineEdit(self)
+        self.textbox.move(76, 40)
+        self.textbox.resize(400, 90)
+        self.setWindowTitle('Directories not Found: Event Configuration Error ')
+        self.textbox.setText("Directory: Purple : Not found in the Directory Path provided \n Directories: Blue, White : Found ")
+
+        self.scan_button = QPushButton('Close', self)
+        self.scan_button.move(225, 160)
+        self.scan_button.clicked.connect(self.close)
