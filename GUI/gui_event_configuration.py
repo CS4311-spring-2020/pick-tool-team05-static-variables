a3f62464e70b7c07c0a0c483b5cd2515157381ef
Manuel Delgado <mdelgado@27@miners.utep.edu>
2020-02-04 14:38:51 -0700

    Corrected event configuration as per SRS. Need to add icon, connect buttons, read save process info.

--- GUI/gui_event_configuration.py
+++ GUI/gui_event_configuration.py
@@ -1,62 +1,38 @@
-from PyQt5.QtWidgets import (QDialog, QVBoxLayout, QDialogButtonBox, QLabel, QLineEdit, QTextEdit, QGridLayout,
-                             QGroupBox, QHBoxLayout, QFormLayout, QScrollArea, QPushButton)
+from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QLabel, QLineEdit, QTextEdit, QGridLayout, QGroupBox, QPushButton)
+from PyQt5.QtGui import QIcon
 
 
-class EventConfiguration(QDialog):
+class EventConfiguration(QWidget):
 
     def __init__(self):
         super(EventConfiguration, self).__init__()
 
-        self.create_horizontal_message()
         self.create_field_box()
-        self.create_vector_box()
         self.show()
-
-        button_options = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
-
-        button_options.accepted.connect(self.accept)
-        button_options.rejected.connect(self.reject)
 
         main_layout = QVBoxLayout()
         main_layout.addWidget(self.grid_fields)
-        main_layout.addWidget(self.form_vector_box)
-        main_layout.addWidget(self.horizontal_message)
-        main_layout.addWidget(button_options)
+
         self.setLayout(main_layout)
 
         self.setWindowTitle("Event Configuration")
-        # self.setWindowIcon()
+        # self.setWindowIcon(QIcon("../Resources/Images/")
 
-    def create_horizontal_message(self):
-        self.horizontal_message = QGroupBox("Message:")
-        layout = QHBoxLayout()
-
-        message = QLabel("Fields with * next to them are required before configuration can begin.")
-        layout.addWidget(message)
-        self.horizontal_message.setLayout(layout)
 
     def create_field_box(self):
-        self.grid_fields = QGroupBox("Fields:")
+        self.grid_fields = QGroupBox("Event Configuration")
         grid = QGridLayout()
         grid.setSpacing(10)
 
-        e_name = QLabel('Event Name* :')
+        e_name = QLabel('Event Name:')
         e_description = QLabel("Event Description:")
-        d_path = QLabel("Directory Path* :")
-        d_one = QLabel("Directory 1* :")
-        d_two = QLabel("Directory 2* :")
-        d_three = QLabel("Directory 3* :")
-        s_date = QLabel("Start Date* :")
-        s_time = QLabel("Start Time* :")
-        e_date = QLabel("End Date* :")
-        e_time = QLabel("End Time* :")
+        s_date = QLabel("Start Date:")
+        s_time = QLabel("Start Time:")
+        e_date = QLabel("End Date:")
+        e_time = QLabel("End Time:")
 
         e_name_edit = QLineEdit()
         e_e_description = QTextEdit()
-        e_d_path = QLineEdit()
-        e_d_one = QLineEdit()
-        e_d_two = QLineEdit()
-        e_d_three = QLineEdit()
         e_s_date = QLineEdit()
         e_s_time = QLineEdit()
         e_e_date = QLineEdit()
@@ -68,43 +44,24 @@ class EventConfiguration(QDialog):
         grid.addWidget(e_description, 2, 0)
         grid.addWidget(e_e_description, 2, 1, 1, 4)
 
-        grid.addWidget(d_path, 3, 0)
-        grid.addWidget(e_d_path, 3, 1, 1, 4)
+        grid.addWidget(s_date, 4, 0)
+        grid.addWidget(e_s_date, 4, 1)
 
-        grid.addWidget(d_one, 4, 0)
-        grid.addWidget(e_d_one, 4, 1, 1, 4)
+        grid.addWidget(s_time, 5, 0)
+        grid.addWidget(e_s_time, 5, 1)
 
-        grid.addWidget(d_two, 5, 0)
-        grid.addWidget(e_d_two, 5, 1, 1, 4)
+        grid.addWidget(e_date, 5, 3)
+        grid.addWidget(e_e_date, 5, 4)
 
-        grid.addWidget(d_three, 6, 0)
-        grid.addWidget(e_d_three, 6, 1, 1, 4)
+        grid.addWidget(e_time, 4, 3)
+        grid.addWidget(e_e_time, 4, 4)
 
-        grid.addWidget(s_date, 7, 0)
-        grid.addWidget(e_s_date, 7, 1)
+        # grid.setColumnStretch(1, 10)
 
-        grid.addWidget(s_time, 8, 0)
-        grid.addWidget(e_s_time, 8, 1)
+        save_event_button = QPushButton("Save Event")
 
-        grid.addWidget(e_date, 7, 3)
-        grid.addWidget(e_e_date, 7, 4)
+        grid.addWidget(save_event_button, 8, 2)
 
-        grid.addWidget(e_time, 8, 3)
-        grid.addWidget(e_e_time, 8, 4)
-
-        grid.setColumnStretch(1, 10)
         self.grid_fields.setLayout(grid)
-
-    def create_vector_box(self):
-        self.form_vector_box = QGroupBox("Vectors:")
-        layout = QFormLayout()
-
-        vector_scroll = QScrollArea()
-        b1 = QPushButton("Add")  # Might want ot resize add button
-        # self.b1.clicked.connect(lamda:self.add_vector(self.b1))
-
-        layout.addWidget(vector_scroll)
-        layout.addWidget(b1)
-        self.form_vector_box.setLayout(layout)
 
         # (TODO) read,Save, and Process information from fields, Change name ok button and connect it
