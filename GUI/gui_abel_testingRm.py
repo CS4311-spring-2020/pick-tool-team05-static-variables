7b0c6f91700f11b2e5f81d9d6cc75de61e01a731
Abel <arodriguez103@miners.utep.edu>
2020-02-03 18:24:05 -0700

    testing random commit

--- GUI/gui_abel_testingRm.py
+++ GUI/gui_abel_testingRm.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import (QApplication, QCheckBox, QGridLayout, QGroupBox,
                              QLabel, QLineEdit, QMessageBox, QRadioButton, QVBoxLayout, QWizard,
                              QWizardPage)
 
+# gjhghjggj
 
 class ClassWizard(QWizard):
     def __init__(self, parent=None):
