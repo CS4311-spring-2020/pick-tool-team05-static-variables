13afe4397b148f54e95147b7d730355ef3647d3a
aleonal <thelifeofantoine@outlook.com>
2020-02-06 06:58:40 -0700

    Added most of main components in main window. Missing node in graphical format requirements, missing connection to other key windows.

--- __init__.py
+++ __init__.py
@@ -2,9 +2,9 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 # To test stuff, call your GUIs here in your branch. If you merge to master, make sure to leave the file like this.
-from GUI.gui_main import Main
+from GUI.Core import MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main = Main()
+    main = MainWindow()
     sys.exit(app.exec_())
