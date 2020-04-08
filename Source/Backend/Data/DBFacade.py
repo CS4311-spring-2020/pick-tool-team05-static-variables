e3a64f2d2ece9700e01d2c23ac42e1153be5c2cc
Elizabeth Barragan <ebarragan3@Miners.utep.edu>
2020-04-07 20:13:40 -0600

    Added basic functions to update and search, and handle document id duplication.

--- Source/Backend/Data/DBFacade.py
+++ Source/Backend/Data/DBFacade.py
@@ -1,4 +1,6 @@
-from pymongo import MongoClient
+from bson import ObjectId
+from pymongo import MongoClient, collection
+
 
 class DBFacade():
     def __init__(self, dbName=None, collectionName=None):
@@ -10,16 +12,31 @@ class DBFacade():
         self.collection = self.db[self.collectionName]
 
     def add(self, data):
-         # Insert Data
+        # Checking for duplicate data within collections
+
+        # Insert Data
         rec_id1 = self.collection.insert_one(data)
         print("Data inserted with record ids", rec_id1, " ")
 
         # Printing the data inserted
         cursor = self.collection.find()
-        for record in cursor:
-            print(record)
 
-    def delete(self,data):
+    # Updates data with a specified ID and the data its going to change it to,
+    # if a document already exists with that ID it will create a new doc
+    def update(self, doc_id, data):
+        document = collection.update_one({'_id': ObjectId(doc_id)}, {"$set": data}, upset=True)
+        return document.acknowledged
+
+    def get_single_data(self, doc_id):
+        data = collection.find_one({'_id': ObjectId(doc_id)})
+        return data
+
+    # Retrieve all the objects inside the collection on a specified criteria
+    def get_multiple_data(self):
+        data = collection.find()
+
+    def delete(self, data):
         pass
-    def find(self,data):
+
+    def find(self, data):
         pass
