489e7b7736110d66cad5fd704f70c8d5699eae19
Elizabeth Barragan <ebarragan3@Miners.utep.edu>
2020-04-09 21:31:25 -0600

    Created tests to send data to Event Configuration

--- Source/Backend/__init__.py
+++ Source/Backend/__init__.py
@@ -1,16 +1,26 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 
-from Source.Backend.Ingestion.SplunkFacade import SplunkFacade
-from Source.Backend.Ingestion.Cleanser import Cleanser
+#from Source.Backend.Ingestion.SplunkFacade import SplunkFacade
+#from Source.Backend.Ingestion.Cleanser import Cleanser
 # from Source.Backend.Ingestion.SplunkFacade import Test
 from Source.Frontend.GraphicalUserInterface import MainWindow
+from Source.Backend.Data.EventConfiguration import EventConfiguration
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    g = MainWindow()
-    test2 = Cleanser()
-    test = SplunkFacade(test2.cpath)
+   #app = QApplication(sys.argv)
+    #g = MainWindow()
+    #test2 = Cleanser()
+    #test = SplunkFacade(test2.cpath)
 
-    sys.exit(app.exec_())
+    # Database Testing
+    ec = EventConfiguration("", "", "HH:MM MM/DD/YY AM/PM", "HH:MM MM/DD/YY AM/PM", 'usr/local/', "usr/local/red",
+                            "usr/local/white", "usr/local/blue", True, "127.0.0.1", 2)
+    #ec = EventConfiguration("", "", "HH:MM MM/DD/YY AM/PM", "HH:MM MM/DD/YY AM/PM", 'usr/local/', "usr/local/red",
+                            #"usr/local/white", "usr/local/blue", False, "127.0.0.1", 2)
+
+
+
+
+    #sys.exit(app.exec_())
