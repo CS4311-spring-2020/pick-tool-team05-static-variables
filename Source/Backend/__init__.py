0af8a8a3a683aeb3efe2dd17fb375c52304911eb
Minikitteh <yamel0727@gmail.com>
2020-04-15 17:07:55 -0600

    adding to code & tests

--- Source/Backend/__init__.py
+++ Source/Backend/__init__.py
@@ -3,16 +3,14 @@ from PyQt5.QtWidgets import QApplication
 
 from Source.Backend.Ingestion.SplunkFacade import SplunkFacade
 from Source.Backend.Ingestion.Cleanser import Cleanser
-from Source.Backend.Ingestion.EnforcementActionReport import EnforcementActionReport
+# from Source.Backend.Ingestion.SplunkFacade import Test
 from Source.Frontend.GraphicalUserInterface import MainWindow
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     #g = MainWindow()
-
     test2 = Cleanser()
     test = SplunkFacade(test2.cpath)
-    test3 = EnforcementActionReport(test.path)
 
     sys.exit(app.exec_())
