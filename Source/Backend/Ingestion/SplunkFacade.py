1af8100f0dd24be8c0344ba3a889a9be8a6295d6
Minikitteh <yamel0727@gmail.com>
2020-04-16 14:27:10 -0600

    failed attempt with Enforcement, BUT imporved other classes

--- Source/Backend/Ingestion/SplunkFacade.py
+++ Source/Backend/Ingestion/SplunkFacade.py
@@ -11,33 +11,32 @@ class SplunkFacade:
         # Hardcoded path
         #self.path = "C:\\Users\\mdelgado\\Desktop\\Log Examples"
         #this is uncleansed path
-        self.path = "C:\\Users\\yamel\\Desktop\\splunk\\tests"
+        #self.path = "C:\\Users\\yamel\\Desktop\\splunk\\tests"
 
         #cleansed
         self.path = cpath
         #self.path = Cleanser.cpath
-
+        print("connecting to splunk")
         # Constants created with log-in information
         HOST = "localhost"
         PORT = 8089
         #USERNAME = "menny" # Individual information needed by Splunk, was thinking of creating a class in the future that will allow the individual user to set that info
-        USERNAME = "Minikitteh"
+        #USERNAME = "Minikitteh"
         #PASSWORD = "Manuel27!"
-        PASSWORD = "Mini111~@"
+        #PASSWORD = "Mini111~@"
 
         # Create a Service instance and log in
         self.service = client.connect(
             host=HOST,
             port=PORT,
-            username=USERNAME,
-            password=PASSWORD)
+            username = (input("input username: ")),
+            password = (input("input password: ")))
+            #username=USERNAME,
+            #password=PASSWORD)
 
         self.upload()
 
         self.logList = []
-
-
-
 
     def get_log_entries(self):
         for items in self.getLogEntries():
@@ -47,37 +46,35 @@ class SplunkFacade:
 
 
     def upload(self):
+
         # Retrieve the index for the data
         current_index = self.service.indexes["main"] # indexes are like folders inside Splunk, you can create new indexes with a command not found here, this is to get an index already existing
+        #count = 1
+
+
         for filename in os.listdir(self.path):
             # Upload and index the file
             try:
                 current_index.upload(self.path + "\\" + filename)
+                print("uploaded file")
+                #count += 1
             except Exception as e:
                 print("Failed to upload, error ", str(e))
 
-    def getLogEntries(self):
+    #def getLogEntries(self):
         # Create a job instance and query the first five events(log entries)
-        job = self.service.jobs.create("search * | head 5")
-        rr = results.ResultsReader(job.preview()) # returns a dictionary with the information inside Splunk
-        log_entries = [] # Creates a list and just append the information with the right keys from Splunk
-        for entry in rr:
-            log_entries.append([entry['_serial'], entry['_raw'], entry['_time'], entry['source']])
-        '''
-        for item in log_entries:
-            print(*item)
-        '''
-        ''''
-        print(log_entries)
-        '''
-        return log_entries
+     #   job = self.service.jobs.create("search * | head 5")
+      #  rr = results.ResultsReader(job.preview()) # returns a dictionary with the information inside Splunk
+       # log_entries = [] # Creates a list and just append the information with the right keys from Splunk
+        #for entry in rr:
+         #   log_entries.append([entry['_serial'], entry['_raw'], entry['_time'], entry['source']])
+        #'''
+        #for item in log_entries:
+         #   print(*item)
+        #'''
+        #''''
+        #print(log_entries)
+        #'''
+        #return log_entries
 
 
-
-#class Test(SplunkFacade): #Just a basic test to make sure information moves correctly.
- #   pass
-#s = SplunkFacade()
-
-#for items in s.getLogEntries():
- #   y = json.dumps(items)
-  #  print(y)
