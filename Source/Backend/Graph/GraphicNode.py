3166b6b6fe3ff8f224608bd9fbe749171ed81b77
Manuel Delgado <mdelgado@27@miners.utep.edu>
2020-04-22 13:49:54 -0600

    Node colors in the contents section of the node.

--- Source/Backend/Graph/GraphicNode.py
+++ Source/Backend/Graph/GraphicNode.py
@@ -7,26 +7,32 @@ class GraphicsNode(QGraphicsItem):
     def __init__(self, node, title="Node Graphics Item", parent=None):
         super().__init__(parent)
 
+        # settings
         # self.title = title
         self._title_color = Qt.black
         self._title_font = QFont("Times New Roman", 12)
-
-        self.initTitle()
-        self.title = title
 
         self.width = 180
         self.height = 240
 
         self.edge_size = 10.0
-        # self.title_height = 24.0
+        self.title_height = 24.0
+
+        # offset from left side
+        self._padding = 10.0
 
         self._pen_default = QPen(QColor("#7f000000"))
         self._pen_selected = QPen(QColor("#FFFFA637"))
 
+        self._brush_title = QBrush(QColor("#FF515151"))
+        self._brush_background = QBrush(QColor("#E3212121"))
+
+        self.initTitle()
+        self.title = title
 
         self.initUI()
 
-        # Give selectable and movable ability to the bounds given to the node
+    # Give selectable and movable ability to the bounds given to the node
     def initUI(self):
         self.setFlag(QGraphicsItem.ItemIsSelectable)
         self.setFlag(QGraphicsItem.ItemIsMovable)
@@ -43,6 +49,14 @@ class GraphicsNode(QGraphicsItem):
         self.title_item = QGraphicsTextItem(self)
         self.title_item.setDefaultTextColor(self._title_color)
         self.title_item.setFont(self._title_font)
+
+        # sets padding to the title offset from the left
+        self.title_item.setPos(self._padding, 0)
+        self.title_item.setTextWidth(
+            self.width
+            - 2
+            * self._padding
+        )
         # self.title_item.setPlainText(self.title)
 
     @property
@@ -55,9 +69,27 @@ class GraphicsNode(QGraphicsItem):
 
     def paint(self, painter, QStyleOptionGraphicsItem, widget=None):
 
-        # title
+        # calculates size of title section and colors the background of the title
+        path_title = QPainterPath()
+        path_title.setFillRule(Qt.WindingFill)
+        path_title.addRoundedRect(0, 0, self.width, self.title_height, self.edge_size, self.edge_size)
+        path_title.addRect(0, self.title_height - self.edge_size, self.edge_size, self.edge_size)
+        path_title.addRect(self.width - self.edge_size, self.title_height - self.edge_size, self.edge_size,
+                           self.edge_size)
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(self._brush_title)
+        painter.drawPath(path_title.simplified())
 
-        # contet
+        # calculates size of body of the node and paints it the background contet
+        path_content = QPainterPath()
+        path_content.setFillRule(Qt.WindingFill)
+        path_content.addRoundedRect(0, self.title_height, self.width, self.height - self.title_height, self.edge_size,
+                                    self.edge_size)
+        path_content.addRect(0, self.title_height, self.edge_size, self.edge_size)
+        path_content.addRect(self.width - self.edge_size, self.title_height, self.edge_size, self.edge_size)
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(self._brush_background)
+        painter.drawPath(path_content.simplified())
 
         # outline
         path_outline = QPainterPath()
