6bb7eff19a54a2586f22f034023067bef2349ac9
Manuel Delgado <mdelgado@27@miners.utep.edu>
2020-04-24 16:52:36 -0600

    Remove unwanted/useless practice code form graph window.

--- Source/Backend/Graph/GraphWindow.py
+++ Source/Backend/Graph/GraphWindow.py
@@ -32,37 +32,6 @@ class GraphWindow(QWidget):
         self.view = GraphicsView(self.scene.grScene, self)
         self.layout.addWidget(self.view)
 
-        # self.addDebugContent()
-
-    def addDebugContent(self):
-
-        greenBrush = QBrush(Qt.green)
-        outlinePen = QPen(Qt.black)
-        outlinePen.setWidth(2)
-
-        # self.grScene.addRect(0, 0, 100, 100, QPen(Qt.red), QBrush(Qt.yellow))
-        rect = self.grScene.addRect(-100, -100, 100, 100, outlinePen, greenBrush)
-        rect.setFlag(QGraphicsItem.ItemIsMovable)
-
-        text = self.grScene.addText("This is my awesome Text")
-        text.setFlag(QGraphicsItem.ItemIsSelectable)
-        text.setFlag(QGraphicsItem.ItemIsMovable)
-        text.setDefaultTextColor(QColor.fromRgbF(1.0, 1.0, 1.0, 1.0))
-
-        widget1 = QPushButton("Hello World")
-        proxy1 = self.grScene.addWidget(widget1)
-        proxy1.setFlag(QGraphicsItem.ItemIsMovable)
-        proxy1.setPos(0, 20)
-
-        widget2 = QTextEdit()
-        proxy2 = self.grScene.addWidget(widget2)
-        proxy2.setFlag(QGraphicsItem.ItemIsSelectable)
-        proxy2.setPos(0, 40)
-
-        line = self.grScene.addLine(-200, -200, 400, -100, outlinePen)
-        line.setFlag(QGraphicsItem.ItemIsMovable)
-        line.setFlag(QGraphicsItem.ItemIsSelectable)
-
     def loadStyleSheet(self, filename):
         print("Style Loading:", filename)
         file = QFile(filename)
