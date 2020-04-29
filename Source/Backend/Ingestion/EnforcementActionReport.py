22d5a235443198d3fb7556426a37243e5c91270e
Minikitteh <yamel0727@gmail.com>
2020-04-29 16:55:51 -0600

    working on ingestion class

---
+++ Source/Backend/Ingestion/EnforcementActionReport.py
@@ -0,0 +1,19 @@
+
+import csv
+import os
+import path
+
+#know log file from ingestion
+#know the errors of the log file from error cless
+
+
+
+
+
+class EnforcementActionReport:
+
+    def __init__(self):
+
+
+
+
--- Source/Backend/__init__.py
+++ Source/Backend/__init__.py
@@ -3,13 +3,14 @@ from PyQt5.QtWidgets import QApplication
 
 from Source.Backend.Ingestion.SplunkFacade import SplunkFacade
 from Source.Backend.Ingestion.Cleanser import Cleanser
-# from Source.Backend.Ingestion.SplunkFacade import Test
+from Source.Backend.Ingestion.EnforcementActionReport import EnforcementActionReport
 from Source.Frontend.GraphicalUserInterface import MainWindow
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     #g = MainWindow()
+    #test3 = EnforcementActionReport()
     test2 = Cleanser()
     test = SplunkFacade(test2.cpath)
 
