9f240790cc1a2dbeca1c436fa6719fbf8cab7bde
Elizabeth Barragan <ebarragan3@Miners.utep.edu>
2020-04-30 19:04:45 -0600

    To test

--- Source/Backend/__init__.py
+++ Source/Backend/__init__.py
@@ -1,41 +1,50 @@
-
 import sys
 from PyQt5.QtWidgets import QApplication
 
 # from Source.Backend.Ingestion.SplunkFacade import SplunkFacade
 # from Source.Backend.Ingestion.Cleanser import Cleanser
 # from Source.Backend.Ingestion.SplunkFacade import Test
-from Source.Frontend.GraphicalUserInterface import MainWindow
 from Source.Backend.Data.EventConfiguration import EventConfiguration
-#from Source.Backend.Data.Vector import Vector
+from Source.Frontend.GraphicalUserInterface import MainWindow
+# from Source.Backend.Data.EventConfiguration import EventConfiguration
+from Source.Backend.Data.Vector import Vector
 
 
 # Database Testing
-def db_tests():
+def db_ec_tests():
     # This information will be coming from the EC window, for testing reasons the dummy data is given here
-    ec = EventConfiguration("AA Attackkkkk", "This is an attack", "HH:MM MM/DD/YY AM/PM", "HH:MM MM/DD/YY AM/PM",
-                            'usr/local/', "usr/local/red","usr/locaffl/white", "usr/qfflocal/blue", False, "127.0.0.1", 4)
-    # Pulling an object from the DB and loading it into the DB.
-    ec.pull_object("Event Name", "S")
-    i = ec.get_name()
+    # Every time a new Object is created it will be inserted into the DB.
+    ec = EventConfiguration("SQL Attack", "This is an SQL attack", "12:49 04/18/20 PM", "11:49 04/18/20 PM",
+                            'usr/local/', "usr/local/red", "usr/local/white", "usr/local/blue", "False", "127.0.0.1",
+                            "4")
+
+    # Pulling an object from the DB and loading it into the system with specified search criteria
+    EventConfiguration.pull_object(EventConfiguration,"Event Name", "S")
+    i = EventConfiguration.get_name(EventConfiguration)
+    # print(i)
+
+
+    i = EventConfiguration.get_name(EventConfiguration)
     print(i)
 
 
-#Vector Database Testing
-#vdb = Vector("Ddos", "attack")
+# Vector Database Testing
+def db_vector_tests():
+    vdb = Vector("Sss", "attack")
+
+
 
 if __name__ == '__main__':
-    #app = QApplication(sys.argv)
-    #g = MainWindow()
-    #sys.exit(app.exec_())
+    # app = QApplication(sys.argv)
+    # g = MainWindow()
+    # sys.exit(app.exec_())
     # test2 = Cleanser()
     # test = SplunkFacade(test2.cpath)
 
-    db_tests()
-
-
-
-
-
+    #db_ec_tests()
+    db_vector_tests()
+    #EventConfiguration.pull_object(EventConfiguration,"Event Name", "A")
+    #s = EventConfiguration.get_start_time(EventConfiguration)
+    #print (s)
 
 
