26be547dfb6585fb7d6c381796d0914243fb2785
Elizabeth Barragan <ebarragan3@Miners.utep.edu>
2020-04-30 21:20:36 -0600

    test

--- Source/Backend/__init__.py
+++ Source/Backend/__init__.py
@@ -11,6 +11,7 @@ from Source.Backend.Data.Vector import Vector
 from Source.Backend.Data.mongo_setup import global_init
 
 
+
 # Database Testing
 def db_ec_tests():
     # This information will be coming from the EC window, for testing reasons the dummy data is given here
@@ -20,10 +21,9 @@ def db_ec_tests():
                             "4")
 
     # Pulling an object from the DB and loading it into the system with specified search criteria
-    EventConfiguration.pull_object(EventConfiguration,"Event Name", "S")
+    EventConfiguration.pull_object(EventConfiguration, "Event Name", "S")
     i = EventConfiguration.get_name(EventConfiguration)
     # print(i)
-
 
     i = EventConfiguration.get_name(EventConfiguration)
     print(i)
@@ -34,7 +34,6 @@ def db_vector_tests():
     vdb = Vector("Sss", "attack")
 
 
-
 if __name__ == '__main__':
     # app = QApplication(sys.argv)
     # g = MainWindow()
@@ -42,11 +41,9 @@ if __name__ == '__main__':
     # test2 = Cleanser()
     # test = SplunkFacade(test2.cpath)
 
-    #db_ec_tests()
-    mongo_setup('PICKDB')
-    db_vector_tests()
-    #EventConfiguration.pull_object(EventConfiguration,"Event Name", "A")
-    #s = EventConfiguration.get_start_time(EventConfiguration)
-    #print (s)
-
-
+    # db_ec_tests()
+    global_init('PICKDB')
+    # db_vector_tests()
+    # EventConfiguration.pull_object(EventConfiguration,"Event Name", "A")
+    # s = EventConfiguration.get_start_time(EventConfiguration)
+    # print (s)
