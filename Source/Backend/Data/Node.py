9ecbce45eaf7a1093e504ed95c69cbae7dc713db
Elizabeth Barragan <ebarragan3@Miners.utep.edu>
2020-05-01 20:35:38 -0600

    Structure to add Vec Subsys on the DB.

--- Source/Backend/Data/Node.py
+++ Source/Backend/Data/Node.py
@@ -1,21 +1,31 @@
+from Source.Backend.Data.SignificantLogEntry import SignificantLogEntry
+from Source.Backend.Data.NodeVisibility import NodeVisibility
+
+
+
 class Node:
-    def __init__(self):
-        self.ID
-        self.name
-        self.time_stamp
-        self.description
-        self.log_entry_reference
-        self.log_creator
-        self.event_type
-        self.icon_type
-        self.source
-        self.node_visibility = True
-        self.ID_visibility = True
-        self.name_visibility = True
-        self.time_stamp_visibility = True
-        self.description_visibility = True
-        self.log_entry_reference_visibility = True
-        self.log_creator_visibility = True
-        self.event_type_visibility = True
-        self.icon_type_visibility = True
-        self.source_visibility = True
+    def __init__(self, node_id, node_name, node_timestamp, node_description, log_creator, event_type, icon_type, source,):
+        self.nodeID = node_id
+        self.nodeName = node_name
+        self.nodeTimestamp = node_timestamp
+        self.nodeDescription = node_description
+        self.logCreator = log_creator
+        self.eventType = event_type
+        self.iconType = icon_type
+        self.source = source
+
+        s = SignificantLogEntry("8","8","ll","","","")
+        n = NodeVisibility(True,True,True,True,True,True,True,True,True,True)
+        self.node = {
+            "Node_ID": self.nodeID,
+            "Node Name": self.nodeName,
+            "Node Timestamp": self.nodeTimestamp,
+            "Node Description": self.nodeDescription,
+            "Log Entry Reference": s.significant_log_entry,
+            "Log Creator": self.logCreator,
+            "Event Type": self.eventType,
+            "Icon Type": self.iconType,
+            "Source": self.source,
+            "Node Visibility": n.node_visibility_
+
+        }
--- Source/Backend/Data/SignificantLogEntry.py
+++ Source/Backend/Data/SignificantLogEntry.py
@@ -1,13 +1,24 @@
-class LogEntry:
-    def __init__(self):
-        self.number
-        self.time_stamp
-        self.content
-        self.host
-        self.source
-        self.type
+from PyQt5.QtWidgets import QTableWidgetItem
+from Source.Backend.Data.DBFacade import add
 
 
+class SignificantLogEntry(QTableWidgetItem):
+    def __init__(self, log_entry_number, log_entry_timestamp, log_entry_content, host, source, source_type):
+        self.logEntryNumber = log_entry_number
+        self.logEntryTimestamp = log_entry_timestamp
+        self.logEntryContent = log_entry_content
+        self.host = host
+        self.source = source
+        self.sourceType = source_type
+
+        self.significant_log_entry = {
+            "Log Entry Number": self.logEntryNumber,
+            "Log Entry Timestamp": self.logEntryTimestamp,
+            "Log Entry Content": self.logEntryContent,
+            "Host": self.host,
+            "Source": self.source,
+            "Source Type": self.sourceType
+        }
 # Not sure who wrote the code and not sure how it will affect the removal of reason for comment
 """
 import inspect
