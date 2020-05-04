34cd92803ac8a35c02829bf687615725720efd1c
Manuel Delgado <mdelgado@27@miners.utep.edu>
2020-05-04 13:07:36 -0600

    Reverted from merge attempt in order to connect the graph to the vector, but I sis not understand the vector completely. Also removed commented out code to make easier when merging into dev.

--- Source/Backend/Data/Vector.py
+++ Source/Backend/Data/Vector.py
@@ -1,7 +1,3 @@
-#from Source.Backend.Data.DBFacade import add
-#from Source.Backend.Data.DBFacade import delete
-# from Source.Backend.Data.DBFacade import search_n
-from Source.Backend.Data.Graph import Graph
 from Source.Backend.Graph.GraphWindow import GraphWindow
 
 DEBUG = True
@@ -12,7 +8,6 @@ class Vector:
         self.name = name
         self.description = description
 
-        #g = Graph("or", 88, 88, "sg", "Node1", "Relationship 2")
         self.graph = GraphWindow(self.name)
 
         if DEBUG: print("Name of vector: ", self.name, " and description: ", self.description)
@@ -23,8 +18,6 @@ class Vector:
             "Description": self.description,
             "Graph": self.graph
         }
-
-        #add("Vector", self.vector)       # adds vector when its created
 
     def get_name(self):
         return self.name
@@ -41,15 +34,3 @@ class Vector:
     def getVector(self):
         return self.vector
 
-    """
-    Function to delete a full vector along its content in the Vector collection in the DB  with
-    specified search values
-    """
-
-    '''
-    def delete(self, attribute, value):
-        delete("Vector", attribute, value)
-    '''
-    # TODO: Pull object ot
-    def pull_objectv(self):
-        pass
