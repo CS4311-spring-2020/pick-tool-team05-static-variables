460e81c789361973ff04a374e10c428b13be27e7
Minikitteh <yamel0727@gmail.com>
2020-05-04 17:08:01 -0600

    trying

--- Source/Backend/Ingestion/LogFile.py
+++ Source/Backend/Ingestion/LogFile.py
@@ -1,31 +1,42 @@
 # Cleansing, validation should be through before the ingestion process should begin and after sent to Splunk Ingestion should be set to true
 # All this information should be saved in the database, where I believe it is through the ingestion facade where it should make the call.
 class LogFile:
-    def __init__(self, name):       # Constructor of a Log File requires a name and sets flags to false
-        self.cleansingStat = False
+    def __init__(self, name, path):       # Constructor of a Log File requires a name and sets flags to false
+        self.name = name
+        self.path = path
+        self.errors = []
+        self.cleansingStat = True
         self.validationStat = False
         self.ingestionStat = False
-        self.acknowledgementStat = self.setAcknowledgementStatus()
-        self.name = name
+        self.acknowledgementStat = False
+        self.invalidStat = False
+
+
 
 # Class  functions to change the status of the attributes of any log file
-    def setCleansingStat(self, stat):
-        self.cleansingStat = stat
+    def setCleansingStat(self):
+        self.cleansingStat = False
 
-    def setValidationStat(self, stat):
-        self.validationStat = stat
+    def setValidationStat(self):
+        self.validationStat = True
 
-    def setIngestionStat(self, stat):
-        self.ingestionStat = stat
+    def setIngestionStat(self):
+        self.ingestionStat = True
 
-# Class function that will force validate, changing all the log file attributes to true
-    def setAcknowledgementStatus(self):
+    def setInvalidStat(self):
+        self.invalidStat = True
+
+    def getAcknowledgementStatus(self):
         if self.cleansingStat and self.validationStat and self.ingestionStat:
-            return True
-        else:
-            return False
+            self.acknowledgementStat = True
 
 # Getters for all the Log File attributes
+    def get_path(self):
+        return self.path
+
+    def get_name(self):
+        return self.name
+
     def getCleansingStat(self):
         return self.cleansingStat
 
@@ -34,3 +45,9 @@ class LogFile:
 
     def getIngestionStat(self):
         return self.ingestionStat
+
+    def getInvalidStat(self):
+        return self.invalidStat
+
+    def insert_errors(self, e):
+        self.errors.append(e)
